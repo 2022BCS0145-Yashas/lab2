0ee349688eb3a3c135a857ee8f7f95945a859d68
YashasKumar <yashaskumar08@gmail.com>
2026-01-09 21:23:42 +0530

    Model=Lasso, alpha=0.1, testsplit=0.2, scaler=true

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import json
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv("dataset/winequality.csv")
@@ -15,7 +15,7 @@ X = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = Ridge(alpha=0.1)
+model = Lasso(alpha=0.1)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
