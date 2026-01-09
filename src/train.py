3049491971fc598ad3d53324d54160d8c46a64f7
YashasKumar <yashaskumar08@gmail.com>
2026-01-09 21:24:42 +0530

    Model=Ridge, alpha=1.0, testsplit=0.2, scaler=false

--- src/train.py
+++ src/train.py
@@ -10,8 +10,9 @@ df = pd.read_csv("dataset/winequality.csv")
 X = df.drop(columns=["quality"])
 y = df["quality"]
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+# scaler = StandardScaler()
+# X = scaler.fit_transform(X)
+X = X.values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
